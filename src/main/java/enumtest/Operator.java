f83f0a6af74dde58579af9a409c12697e33af722
lim4158 <lim4158@gmail.com>
2023-05-01 17:38:48 +0900

    [Create] enum Test 예제 추가

---
+++ src/main/java/enumtest/Operator.java
@@ -0,0 +1,22 @@
+package enumtest;
+
+import java.util.function.BiFunction;
+
+public enum Operator {
+    PLUS("덧셈", (a, b) -> (a + b)),
+    MINUS("뺄셈", (a, b) -> (a - b)),
+    MULTIPLY("곱셈", (a, b) -> (a * b)),
+    DIVIDE("나눗셈", (a, b) -> (a / b));
+
+    private final String name;
+    private final BiFunction<Double, Double, Double> biFunction;
+
+    Operator(String name, BiFunction<Double, Double, Double> biFunction) {
+        this.name = name;
+        this.biFunction = biFunction;
+    }
+
+    public Double calculate(double a, double b) {
+        return this.biFunction.apply(a, b);
+    }
+}
