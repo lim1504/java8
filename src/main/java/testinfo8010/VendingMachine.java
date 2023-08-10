87fbafe65d8a02cc6031ec0c52aa08a89e780e7a
lim4158 <lim4158@gmail.com>
2023-08-10 23:06:44 +0900

    [Create] 자바 클래스 및 메소드 사용, 인스턴스 메모리 , stack, perm, heap에 대한 이해 - 1

---
+++ src/main/java/testinfo8010/VendingMachine.java
@@ -0,0 +1,18 @@
+package testinfo8010;
+
+public class VendingMachine {
+    // field 영역
+
+    // 생성자
+
+    // method
+
+    public String getProduct(int productNumber) {
+
+        return "콜라";
+    }
+
+    public static void getVer() {
+        System.out.println("v1.0");
+    }
+}
