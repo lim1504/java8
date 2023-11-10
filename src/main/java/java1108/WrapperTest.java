8df2cd96162ec7b75c6439684c5678eac79fc54f
lim4158 <lim4158@gmail.com>
2023-11-10 15:40:16 +0900

    [Create] Wrapper simple test

---
+++ src/main/java/java1108/WrapperTest.java
@@ -0,0 +1,17 @@
+package java1108;
+
+public class WrapperTest {
+    public static void main(String[] args) {
+
+        int a = 1;
+        Integer b = 2;
+        Integer c = null;
+        String bString = b.toString();
+
+        //오토박싱 & 언 박싱 예제
+        Integer c1 = 10;
+        long b2 = 1000L;
+        double c2 = 1.1d;
+    }
+}
+
