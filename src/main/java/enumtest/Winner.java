0637c7c5cd846aa47f11edf1750d8058c6061130
lim4158 <lim4158@gmail.com>
2023-11-26 13:22:07 +0900

    [Update] import 오류 수정

--- src/main/java/enumtest/Winner.java
+++ src/main/java/enumtest/Winner.java
@@ -1,6 +1,5 @@
 package enumtest;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/java1113/OverridingTest.java
+++ src/main/java/java1113/OverridingTest.java
@@ -1,28 +1,27 @@
-package java1113;
-
-import com.sun.xml.internal.ws.org.objectweb.asm.ClassAdapter;
-
-public class OverridingTest {
-    public static void main(String[] args) {
-        ClassInfo1 c1 = new ClassInfo1();
-        c1.methodInfo();
-
-        ClassInfo2 c2 = new ClassInfo2();
-        c2.methodInfo();
-    }
-}
-
-class ClassInfo1 {
-
-    public void methodInfo () {
-        System.out.println("Supper Class -> Method Call");
-    }
-}
-
-class ClassInfo2 extends ClassInfo1{
-
-    @Override
-    public void methodInfo() {
-        System.out.println("Sub Class -> Super Class Method Call");
-    }
-}
+//package java1113;
+//
+//import org.objectweb.asm.ClassAdapter;
+//public class OverridingTest {
+//    public static void main(String[] args) {
+//        ClassInfo1 c1 = new ClassInfo1();
+//        c1.methodInfo();
+//
+//        ClassInfo2 c2 = new ClassInfo2();
+//        c2.methodInfo();
+//    }
+//}
+//
+//class ClassInfo1 {
+//
+//    public void methodInfo () {
+//        System.out.println("Supper Class -> Method Call");
+//    }
+//}
+//
+//class ClassInfo2 extends ClassInfo1{
+//
+//    @Override
+//    public void methodInfo() {
+//        System.out.println("Sub Class -> Super Class Method Call");
+//    }
+//}
