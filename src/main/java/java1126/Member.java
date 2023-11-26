6b60c05f94f6fa9816617828bd9a155b101145f8
lim4158 <lim4158@gmail.com>
2023-11-26 13:22:14 +0900

    [Create] 자바 직렬화 Simple 예제 생성

---
+++ src/main/java/java1126/Member.java
@@ -0,0 +1,36 @@
+package java1126;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+    public String name;
+    public String id;
+    public int age;
+
+    public Member(String name, String id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
